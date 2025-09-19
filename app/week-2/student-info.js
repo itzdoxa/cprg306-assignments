587a146450fe1ef7e7bcaab373f0a515ae1affc0
Deep <153353785+itzdoxa@users.noreply.github.com>
2025-09-18 22:18:01 -0600

    initial commit

---
+++ app/week-2/student-info.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Student Information</h1>
+      <p>Deepinder Singh</p>
+      <p>
+        GitHub Repo:{" "}
+        <Link
+          href=""
+        >
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
